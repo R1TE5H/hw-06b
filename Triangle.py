5a06f4e846575436bc2eb2e7249f04119301404d
Ritesh Persaud <riteshpersaud2003@gmail.com>
2025-10-26 18:19:33 -0400

    Added fixes for bugs (Part 2)

--- Triangle.py
+++ Triangle.py
@@ -27,31 +27,32 @@ def classifyTriangle(a,b,c):
       BEWARE: there may be a bug or two in this code
     """
 
-    # require that the input values be >= 0 and <= 200
+    # First, check that all inputs are integers
+    if not (isinstance(a, int) and isinstance(b, int) and isinstance(c, int)):
+        return 'InvalidInput'
+
+    # Check that all sides are in the valid range
+    if a <= 0 or b <= 0 or c <= 0:
+        return 'InvalidInput'
     if a > 200 or b > 200 or c > 200:
         return 'InvalidInput'
-        
-    if a <= 0 or b <= b or c <= 0:
-        return 'InvalidInput'
-    
-    # verify that all 3 inputs are integers  
-    # Python's "isinstance(object,type) returns True if the object is of the specified type
-    if not(isinstance(a,int) and isinstance(b,int) and isinstance(c,int)):
-        return 'InvalidInput';
-      
-    # This information was not in the requirements spec but 
-    # is important for correctness
-    # the sum of any two sides must be strictly less than the third side
-    # of the specified shape is not a triangle
-    if (a >= (b - c)) or (b >= (a - c)) or (c >= (a + b)):
+
+    # Check for triangle validity: sum of any two sides must be greater than the third
+    if (a + b <= c) or (a + c <= b) or (b + c <= a):
         return 'NotATriangle'
-        
-    # now we know that we have a valid triangle 
-    if a == b and b == a:
-        return 'Equilateral'
-    elif ((a * 2) + (b * 2)) == (c * 2):
+
+    # Check for right triangle
+    sides = sorted([a, b, c])
+    if sides[0]**2 + sides[1]**2 == sides[2]**2:
         return 'Right'
-    elif (a != b) and  (b != c) and (a != b):
-        return 'Scalene'
-    else:
+
+    # Check for equilateral
+    if a == b == c:
+        return 'Equilateral'
+
+    # Check for isosceles
+    if a == b or b == c or a == c:
         return 'Isoceles'
+
+    # Otherwise, scalene
+    return 'Scalene'
