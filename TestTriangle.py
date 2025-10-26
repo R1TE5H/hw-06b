16f209fc0b94e80a93b8f3870b0acbab09f6665f
Ritesh Persaud <riteshpersaud2003@gmail.com>
2025-10-26 18:31:28 -0400

    Updated to fix linting errors

--- TestTriangle.py
+++ TestTriangle.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 """
 Updated Jan 21, 2018
-The primary goal of this file is to demonstrate a simple unittest implementation
+The primary goal of this file is to demonstrate a simple unittest
+implementation
 
 @author: jrr
 @author: rk
@@ -12,7 +13,9 @@ import unittest
 from Triangle import classifyTriangle
 
 # This code implements the unit test functionality
-# https://docs.python.org/3/library/unittest.html has a nice description of the framework
+# https://docs.python.org/3/library/unittest.html has a nice description of
+# the framework
+
 
 class TestTriangles(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
@@ -23,9 +26,11 @@ class TestTriangles(unittest.TestCase):
         Input: (3, 4, 5)
         Expected Results: 'Right'
         Actual Result: classifyTriangle(3,4,5)
-        
+
         """
-        self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
+        self.assertEqual(
+            classifyTriangle(3, 4, 5), "Right", "3,4,5 is a Right triangle"
+        )
 
     def testRightTriangleB(self):
         """
@@ -35,8 +40,10 @@ class TestTriangles(unittest.TestCase):
         Actual Result: classifyTriangle(5,3,4)
 
         """
-        self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
-        
+        self.assertEqual(
+            classifyTriangle(5, 3, 4), "Right", "5,3,4 is a Right triangle"
+        )
+
     def testEquilateralTriangles(self):
         """
         Test ID: 3
@@ -45,8 +52,12 @@ class TestTriangles(unittest.TestCase):
         Actual Result: classifyTriangle(1,1,1)
 
         """
-        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
-    
+        self.assertEqual(
+            classifyTriangle(1, 1, 1),
+            "Equilateral",
+            "1,1,1 should be equilateral",
+        )
+
     def testIsoscelesTriangleA(self):
         """
         Test ID: 4a
@@ -55,7 +66,9 @@ class TestTriangles(unittest.TestCase):
         Actual Result: classifyTriangle(5,5,3)
 
         """
-        self.assertEqual(classifyTriangle(5,5,3),'Isoceles','5,5,3 should be isosceles')
+        self.assertEqual(
+            classifyTriangle(5, 5, 3), "Isoceles", "5,5,3 should be isosceles"
+        )
 
     def testIsoscelesTriangleB(self):
         """
@@ -65,7 +78,9 @@ class TestTriangles(unittest.TestCase):
         Actual Result: classifyTriangle(3,5,5)
 
         """
-        self.assertEqual(classifyTriangle(3,5,5),'Isoceles','3,5,5 should be isosceles')
+        self.assertEqual(
+            classifyTriangle(3, 5, 5), "Isoceles", "3,5,5 should be isosceles"
+        )
 
     def testIsoscelesTriangleC(self):
         """
@@ -75,17 +90,21 @@ class TestTriangles(unittest.TestCase):
         Actual Result: classifyTriangle(5,3,5)
 
         """
-        self.assertEqual(classifyTriangle(5,3,5),'Isoceles','5,3,5 should be isosceles')
-    
+        self.assertEqual(
+            classifyTriangle(5, 3, 5), "Isoceles", "5,3,5 should be isosceles"
+        )
+
     def testScaleneTriangleA(self):
         """
         Test ID: 5a
         Input: (4, 2, 3)
         Expected Results: 'Scalene'
         Actual Result: classifyTriangle(4,2,3)
-        
+
         """
-        self.assertEqual(classifyTriangle(4,2,3),'Scalene','4,2,3 should be scalene')
+        self.assertEqual(
+            classifyTriangle(4, 2, 3), "Scalene", "4,2,3 should be scalene"
+        )
 
     def testScaleneTriangleB(self):
         """
@@ -93,19 +112,25 @@ class TestTriangles(unittest.TestCase):
         Input: (6, 7, 8)
         Expected Results: 'Scalene'
         Actual Result: classifyTriangle(6,7,8)
-        
+
         """
-        self.assertEqual(classifyTriangle(6,7,8),'Scalene','6,7,8 should be scalene')
-    
+        self.assertEqual(
+            classifyTriangle(6, 7, 8), "Scalene", "6,7,8 should be scalene"
+        )
+
     def testInvalidInputA(self):
         """
         Test ID: 6a
         Input: (-1, 2, 3)
         Expected Results: 'InvalidInput'
         Actual Result: classifyTriangle(-1,2,3)
-        
+
         """
-        self.assertEqual(classifyTriangle(-1,2,3),'InvalidInput','Negative side should be invalid')
+        self.assertEqual(
+            classifyTriangle(-1, 2, 3),
+            "InvalidInput",
+            "Negative side should be invalid",
+        )
 
     def testInvalidInputB(self):
         """
@@ -113,9 +138,13 @@ class TestTriangles(unittest.TestCase):
         Input: (0, 2, 3)
         Expected Results: 'InvalidInput'
         Actual Result: classifyTriangle(0,2,3)
-        
+
         """
-        self.assertEqual(classifyTriangle(0,2,3),'InvalidInput','Zero side should be invalid')
+        self.assertEqual(
+            classifyTriangle(0, 2, 3),
+            "InvalidInput",
+            "Zero side should be invalid",
+        )
 
     def testInvalidInputC(self):
         """
@@ -123,9 +152,13 @@ class TestTriangles(unittest.TestCase):
         Input: (201, 2, 3)
         Expected Results: 'InvalidInput'
         Actual Result: classifyTriangle(201,2,3)
-        
+
         """
-        self.assertEqual(classifyTriangle(201,2,3),'InvalidInput','Side > 200 should be invalid')
+        self.assertEqual(
+            classifyTriangle(201, 2, 3),
+            "InvalidInput",
+            "Side > 200 should be invalid",
+        )
 
     def testInvalidInputD(self):
         """
@@ -133,9 +166,13 @@ class TestTriangles(unittest.TestCase):
         Input: (2.5, 2, 2)
         Expected Results: 'InvalidInput'
         Actual Result: classifyTriangle(2.5,2,2)
-        
+
         """
-        self.assertEqual(classifyTriangle(2.5,2,2),'InvalidInput','Non-integer side should be invalid')
+        self.assertEqual(
+            classifyTriangle(2.5, 2, 2),
+            "InvalidInput",
+            "Non-integer side should be invalid",
+        )
 
     def testInvalidInputE(self):
         """
@@ -143,19 +180,27 @@ class TestTriangles(unittest.TestCase):
         Input: ('a', 2, 2)
         Expected Results: 'InvalidInput'
         Actual Result: classifyTriangle('a',2,2)
-        
+
         """
-        self.assertEqual(classifyTriangle('a',2,2),'InvalidInput','Non-integer side should be invalid')
-    
+        self.assertEqual(
+            classifyTriangle("a", 2, 2),
+            "InvalidInput",
+            "Non-integer side should be invalid",
+        )
+
     def testNotATriangleA(self):
         """
         Test ID: 7a
         Input: (1, 2, 3)
         Expected Results: 'NotATriangle'
         Actual Result: classifyTriangle(1,2,3)
-        
+
         """
-        self.assertEqual(classifyTriangle(1,2,3),'NotATriangle','1,2,3 cannot form a triangle')
+        self.assertEqual(
+            classifyTriangle(1, 2, 3),
+            "NotATriangle",
+            "1,2,3 cannot form a triangle",
+        )
 
     def testNotATriangleB(self):
         """
@@ -163,9 +208,13 @@ class TestTriangles(unittest.TestCase):
         Input: (10, 1, 1)
         Expected Results: 'NotATriangle'
         Actual Result: classifyTriangle(10,1,1)
-        
+
         """
-        self.assertEqual(classifyTriangle(10,1,1),'NotATriangle','10,1,1 cannot form a triangle')
+        self.assertEqual(
+            classifyTriangle(10, 1, 1),
+            "NotATriangle",
+            "10,1,1 cannot form a triangle",
+        )
 
     def testNotATriangleC(self):
         """
@@ -173,9 +222,13 @@ class TestTriangles(unittest.TestCase):
         Input: (1, 10, 1)
         Expected Results: 'NotATriangle'
         Actual Result: classifyTriangle(1,10,1)
-        
+
         """
-        self.assertEqual(classifyTriangle(1,10,1),'NotATriangle','1,10,1 cannot form a triangle')
+        self.assertEqual(
+            classifyTriangle(1, 10, 1),
+            "NotATriangle",
+            "1,10,1 cannot form a triangle",
+        )
 
     def testNotATriangleD(self):
         """
@@ -183,19 +236,27 @@ class TestTriangles(unittest.TestCase):
         Input: (1, 1, 10)
         Expected Results: 'NotATriangle'
         Actual Result: classifyTriangle(1,1,10)
-        
+
         """
-        self.assertEqual(classifyTriangle(1,1,10),'NotATriangle','1,1,10 cannot form a triangle')
-    
+        self.assertEqual(
+            classifyTriangle(1, 1, 10),
+            "NotATriangle",
+            "1,1,10 cannot form a triangle",
+        )
+
     def testEdgeCaseEquilateral(self):
         """
         Test ID: 8a
         Input: (200, 200, 200)
         Expected Results: 'Equilateral'
         Actual Result: classifyTriangle(200,200,200)
-        
+
         """
-        self.assertEqual(classifyTriangle(200,200,200),'Equilateral','200,200,200 should be equilateral')
+        self.assertEqual(
+            classifyTriangle(200, 200, 200),
+            "Equilateral",
+            "200,200,200 should be equilateral",
+        )
 
     def testEdgeCaseIsosceles(self):
         """
@@ -203,12 +264,15 @@ class TestTriangles(unittest.TestCase):
         Input: (200, 199, 199)
         Expected Results: 'Isoceles'
         Actual Result: classifyTriangle(200,199,199)
-        
+
         """
-        self.assertEqual(classifyTriangle(200,199,199),'Isoceles','200,199,199 should be isosceles')
+        self.assertEqual(
+            classifyTriangle(200, 199, 199),
+            "Isoceles",
+            "200,199,199 should be isosceles",
+        )
 
 
-if __name__ == '__main__':
-    print('Running unit tests')
+if __name__ == "__main__":
+    print("Running unit tests")
     unittest.main()
-
--- Triangle.py
+++ Triangle.py
@@ -3,56 +3,61 @@
 Created on Thu Jan 14 13:44:00 2016
 Updated Jan 21, 2018
 
-The primary goal of this file is to demonstrate a simple python program to classify triangles
+The primary goal of this file is to demonstrate a simple python program to
+classify triangles
 
 @author: jrr
 @author: rk
 """
 
-def classifyTriangle(a,b,c):
+
+def classifyTriangle(a, b, c):
     """
-    Your correct code goes here...  Fix the faulty logic below until the code passes all of 
-    you test cases. 
-    
-    This function returns a string with the type of triangle from three integer values
-    corresponding to the lengths of the three sides of the Triangle.
-    
+    Your correct code goes here...  Fix the faulty logic below until the code
+    passes all of you test cases.
+
+    This function returns a string with the type of triangle from three
+    integer values corresponding to the lengths of the three sides of the
+    Triangle.
+
     return:
         If all three sides are equal, return 'Equilateral'
         If exactly one pair of sides are equal, return 'Isoceles'
         If no pair of  sides are equal, return 'Scalene'
         If not a valid triangle, then return 'NotATriangle'
-        If the sum of any two sides equals the squate of the third side, then return 'Right'
-      
+        If the sum of any two sides equals the squate of the third side, then
+        return 'Right'
+
       BEWARE: there may be a bug or two in this code
     """
 
     # First, check that all inputs are integers
     if not (isinstance(a, int) and isinstance(b, int) and isinstance(c, int)):
-        return 'InvalidInput'
+        return "InvalidInput"
 
     # Check that all sides are in the valid range
     if a <= 0 or b <= 0 or c <= 0:
-        return 'InvalidInput'
+        return "InvalidInput"
     if a > 200 or b > 200 or c > 200:
-        return 'InvalidInput'
+        return "InvalidInput"
 
-    # Check for triangle validity: sum of any two sides must be greater than the third
+    # Check for triangle validity: sum of any two sides must be greater than
+    # the third
     if (a + b <= c) or (a + c <= b) or (b + c <= a):
-        return 'NotATriangle'
+        return "NotATriangle"
 
     # Check for right triangle
     sides = sorted([a, b, c])
-    if sides[0]**2 + sides[1]**2 == sides[2]**2:
-        return 'Right'
+    if sides[0] ** 2 + sides[1] ** 2 == sides[2] ** 2:
+        return "Right"
 
     # Check for equilateral
     if a == b == c:
-        return 'Equilateral'
+        return "Equilateral"
 
     # Check for isosceles
     if a == b or b == c or a == c:
-        return 'Isoceles'
+        return "Isoceles"
 
     # Otherwise, scalene
-    return 'Scalene'
+    return "Scalene"
